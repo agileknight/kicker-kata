5d36dc3724229d97bf979eee629f13ee3102c60f
Philipp Meier <philipp.meier@codecentric.de>
2014-02-28 17:29:21 +0100

    first dummy test of kicker

--- src/kicker/core.clj
+++ src/kicker/core.clj
@@ -1,18 +1,27 @@
 (ns kicker.core
   (:gen-class))
 
+(defn start-matches?
+  [input [& patterns]]
+  (true? (some #(.startsWith input %) patterns)))
+
 (defn create-event-captor
   []
   (let [captured (atom [])]
-    {:event (fn [event] (swap! captured conj event))
-     :events-matching (fn [pattern] (filter #(if (= :all pattern) true (.startsWith % pattern)) @captured))
+    {:event (fn [event bus] (swap! captured conj event))
+     :events-matching (fn [pattern] (filter #(if (= :all pattern) true (start-matches? % [pattern])) @captured))
      :wants? (fn [event] true)}))
 
 (defn create-bus
   [[& listeners]]
-  {:event (fn [event] (doseq [listener listeners]
+  {:event (fn [event bus] (doseq [listener listeners]
                        (if ((:wants? listener) event)
-                         ((:event listener) event))))})
+                         ((:event listener) event bus))))})
+
+(defn create-kicker
+  []
+  {:event (fn [event bus] ((:event bus) "score:{'black':1, 'white':0}" bus))
+   :wants? (fn [event] (start-matches? event ["goal"]))})
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/kicker/core_test.clj
+++ test/kicker/core_test.clj
@@ -4,12 +4,22 @@
 
 (deftest events-matching-test
   (testing "All events returned when pattern is :all."
-    (let [event-captor (create-event-captor) bus (create-bus [event-captor])]
-      ((:event bus) "red")
-      ((:event bus) "blue")
+    (let [event-captor (create-event-captor)
+          bus (create-bus [event-captor])]
+      ((:event bus) "red" bus)
+      ((:event bus) "blue" bus)
       (is (= ["red" "blue"] ((:events-matching event-captor) :all)))))
   (testing "Matching events returned when pattern is given."
-    (let [event-captor (create-event-captor) bus (create-bus [event-captor])]
-      ((:event bus) "red")
-      ((:event bus) "blue")
+    (let [event-captor (create-event-captor)
+          bus (create-bus [event-captor])]
+      ((:event bus) "red" bus)
+      ((:event bus) "blue" bus)
       (is (= ["blue"] ((:events-matching event-captor) "blue"))))))
+
+(deftest integration-test
+  (testing "First goal triggers score."
+    (let [event-captor (create-event-captor)
+          kicker (create-kicker)
+          bus (create-bus [event-captor kicker])]
+      ((:event bus) "goal:black" bus)
+      (is (= ["score:{'black':1, 'white':0}"] ((:events-matching event-captor) "score"))))))
