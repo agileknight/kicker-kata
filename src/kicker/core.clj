f0e35941a883b25e9766be0c0c4deef5cac36817
Philipp Meier <philipp.meier@codecentric.de>
2014-02-28 16:46:51 +0100

    added event matching to listener

--- src/kicker/core.clj
+++ src/kicker/core.clj
@@ -5,12 +5,14 @@
   []
   (let [captured (atom [])]
     {:event (fn [event] (swap! captured conj event))
-     :events-matching (fn [pattern] @captured)}))
+     :events-matching (fn [pattern] @captured)
+     :wants? (fn [event] true)}))
 
 (defn create-bus
   [[& listeners]]
   {:event (fn [event] (doseq [listener listeners]
-                              ((:event listener) event)))})
+                       (if ((:wants? listener) event)
+                         ((:event listener) event))))})
 
 (defn -main
   "I don't do a whole lot ... yet."
