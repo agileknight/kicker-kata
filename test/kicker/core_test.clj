0655b967dfc5006329bb884c1b5914d4bd9d7428
Philipp Meier <philipp.meier@codecentric.de>
2014-04-18 13:50:22 +0200

    eliminated all tests but the acceptance tests

--- test/kicker/core_test.clj
+++ test/kicker/core_test.clj
@@ -2,75 +2,6 @@
   (:require [clojure.test :refer :all]
             [kicker.core :refer :all]))
 
-(deftest events-matching-test
-  (testing "All events returned when pattern is :all."
-    (let [event-captor (create-event-captor)
-          bus (create-bus [event-captor])]
-      ((:event bus) "red" bus)
-      ((:event bus) "blue" bus)
-      (is (= ["red" "blue"] ((:events-matching event-captor) :all)))))
-  (testing "Matching events returned when pattern is given."
-    (let [event-captor (create-event-captor)
-          bus (create-bus [event-captor])]
-      ((:event bus) "red" bus)
-      ((:event bus) "blue" bus)
-      (is (= ["blue"] ((:events-matching event-captor) "blue"))))))
-
-(deftest increase-score-test
-  (testing "Increases score by 1."
-    (is (= {:black 1 :white 0} (increase-score {:black 0 :white 0} :black)))
-    (is (= {:black 2 :white 0} (increase-score {:black 1 :white 0} :black)))
-    (is (= {:black 0 :white 3} (increase-score {:black 0 :white 2} :white)))))
-
-(deftest score-counter-test
-  (testing "Initially has zero score."
-    (let [counter (make-score-counter)]
-      (is (= {:black 0 :white 0} ((:score counter))))))
-  (testing "One black goal"
-    (let [counter (make-score-counter)]
-      ((:goal counter) :black)
-      (is (= {:black 1 :white 0} ((:score counter))))))
-  (testing "Reset resets score"
-    (let [counter (make-score-counter)]
-      ((:goal counter) :black)
-      ((:reset counter))
-      (is (= {:black 0 :white 0} ((:score counter)))))))
-
-(deftest player-board-test
-  (testing "Registers a player on the board"
-    (let [board (make-player-board)]
-      (is (= {"John" 0} ((:register board) "John")))))
-  (testing "Player wins a game"
-    (let [board (make-player-board)]
-      ((:register board) "John")
-      (is (= {"John" 1} ((:win board) "John")))))
-  )
-
-(deftest team-info-test
-  (testing "Both positions registered in team"
-    (let [team-info (make-team-info)]
-      (register team-info :black :offense "John")
-      (register team-info :black :defense "Jack")
-      (is (= {:black {:offense "John" :defense "Jack"}} (current-teams team-info))))))
-
-(defn make-callback-mock
-  []
-  (let [was-called (atom false)]
-    {:call (fn [] (reset! was-called true))
-     :was-called? (fn [] @was-called)}))
-
-(deftest game-listener-test
-  (testing "Does nothing when game does not end"
-    (let [callback (make-callback-mock)
-          game-listener (make-game-listener (:call callback))]
-      ((:score-changed game-listener) {:black 5 :white 5})
-      (is (false? ((:was-called? callback))))))
-  (testing "Resets score counter when game ends"
-    (let [callback (make-callback-mock)
-          game-listener (make-game-listener (:call callback))]
-      ((:score-changed game-listener) {:black 6 :white 4})
-      (is (true? ((:was-called? callback)))))))
-
 (defn goals
   [stats teams]
   (doall (map #(goal stats %) teams)))
@@ -79,26 +10,47 @@
   [stats team]
   (goals stats (take 6 (repeat team))))
 
-(deftest goal-event-parsing-test
-  (testing "parse goal events correctly"
-    (is (= :black (parse-goal-event "goal:black")))
-    (is (= :white (parse-goal-event "goal:white")))))
+(defn make-capturer
+  []
+  (let [events (atom [])]
+    (reify
+      EventListener
+      (fire [this event] (swap! events conj event))
+      EventReader
+      (latest-events [this] (let [result @events]
+                              (reset! events [])
+                              result)))))
 
-(deftest event-printing-test
-  (testing "print score event correctly"
-    (is (= "score:{'black':1, 'white':3}" (print-event {:type "score" :content {:black 1 :white 3}})))))
+(def ^:dynamic *score-event-capturer*)
 
-(deftest system-test
-  (testing "First goal triggers score."
-    (let [event-captor (create-event-captor)
-          kicker (create-kicker)
-          bus (create-bus [event-captor kicker])]
-      ((:event bus) "goal:black" bus)
-      (is (= ["score:{'black':1, 'white':0}"] ((:events-matching event-captor) "score")))))
-  (testing "Kicker counts score for team."
-    (let [event-captor (create-event-captor)
-          kicker (create-kicker)
-          bus (create-bus [event-captor kicker])]
-      ((:event bus) "goal:black" bus)
-      ((:event bus) "goal:black" bus)
-      (is (= "score:{'black':2, 'white':0}" (last ((:events-matching event-captor) "score")))))))
+(defn setup-score-event-capturer
+  [f]
+  (binding [*score-event-capturer* (make-capturer)]
+    (f)))
+
+(def ^:dynamic *stats*)
+
+(defn setup-statistics-instance
+  [f]
+  (binding [*stats* (make-statistics *score-event-capturer*)]
+    (f)))
+
+(use-fixtures :each setup-score-event-capturer setup-statistics-instance)
+
+(deftest acceptance-test
+  (testing "Goal triggers score."
+    (goal *stats* :black)
+    (goal *stats* :black)
+    (is (= [{:black 1 :white 0}
+            {:black 2 :white 0}] (latest-events *score-event-capturer*))))
+  (testing "New game starts when one team gets 6 goals"
+    (goals *stats* (take 7 (repeat :black)))
+    (is (= {:black 1 :white 0} (last (latest-events *score-event-capturer*)))))
+  '(testing "Prints player stats after team wins"
+     (let [stats (make-statistics)]
+       (register stats :black :offense "Joe")
+       (register stats :black :defense "Jack")
+       (register stats :white :offnse "Daniel")
+       (register stats :white :defense "Dennis")
+       (is (= {:type "player-ranking" :content {"Joe" 1 "Jack" 1 "Daniel" 0 "Dennis" 0}}
+              (last (wins-game stats :black)))))))
