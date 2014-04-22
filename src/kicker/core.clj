4d6837d8579466ea6593d1ce0cba21ec68c87222
Philipp Meier <philipp.meier@codecentric.de>
2014-04-22 21:36:40 +0200

    revert using protocls for internal code

--- src/kicker/core.clj
+++ src/kicker/core.clj
@@ -1,5 +1,4 @@
-(ns kicker.core
-  (:gen-class))
+(ns kicker.core)
 
 (defprotocol EventReader
   "Read latest events."
@@ -9,21 +8,10 @@
   "Listens to events."
   (fire [this event]))
 
-(defprotocol RegistrationListener
-  "Listens to registration events."
-  (register [this team position player-name]))
-
-(defprotocol GoalEventListener
-  "Listens to goal events."
+(defprotocol StatisticsModule
+  "API of the statistics module."
+  (register [this team position player-name])
   (goal [this team]))
-
-(defprotocol GameEndListener
-  "Listens to end of game events."
-  (end-of-game [this winner-team]))
-
-(defprotocol TeamInfo
-  "Current team composition."
-  (current-teams [this]))
 
 (defn increase-in-map
   [map key]
@@ -36,21 +24,16 @@
 (defn make-player-board
   []
   (let [board (atom {})]
-    (reify
-      RegistrationListener
-      (register [this team position player-name] (swap! board assoc name 0))
-      GameEndListener
-      (end-of-game [this winner-team] (do (swap! board increase-in-map (:offense winner-team))
-                                          (swap! board increase-in-map (:defense winner-team)))))
-    ))
+    {:register (fn [team position player-name] (swap! board assoc name 0))
+
+     :end-of-game (fn [winner-team] (do (swap! board increase-in-map (:offense winner-team))
+                                       (swap! board increase-in-map (:defense winner-team))))}))
 
 (defn make-team-info
   []
   (let [team-state (atom {})]
-    (reify RegistrationListener
-      (register [this team position player-name] (swap! team-state assoc-in [team position] player-name))
-      TeamInfo
-    (current-teams [this] @team-state))))
+    ({:register (fn [team position player-name] (swap! team-state assoc-in [team position] player-name))
+      :current-teams (fn [] @team-state)})))
 
 (defn make-score-counter
   [score-event-listener]
@@ -73,9 +56,7 @@
 
         game-listener (make-game-listener (fn [] ((:reset score-counter))))]
     (reify
-      GoalEventListener
+      StatisticsModule
       (goal [this team] (let [new-score ((:goal score-counter) team)]
-                          ((:score-changed game-listener) new-score)
-                          [{:type "score" :content new-score}]))
-      RegistrationListener
+                          ((:score-changed game-listener) new-score)))
       (register [this team position player-name] :todo))))
